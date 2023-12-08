2c577a3c07b8601609a3c9e6afc8d32a38108d7e
zach <102389340+zw0ng@users.noreply.github.com>
2023-12-07 22:10:05 -0800

    fixed error where correct post was showing but author name was undefined

--- frontend/src/pages/Post/PostDetails.page.jsx
+++ frontend/src/pages/Post/PostDetails.page.jsx
@@ -1,22 +1,31 @@
-import { useLoaderData, useNavigate, useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import DOMAIN from "../../services/endpoint";
 import axios from "axios";
 import ArticleCard from "../../components/misc/ArticleCard";
-import { useState, useEffect } from "react";
+import { getAccessToken } from "../../services/jwt.service";
 
 function PostDetailsPage() {
   const responseData = useLoaderData();
   const authorData = responseData[1];
   const postData = responseData[0];
   const { id } = useParams();
+
+  const jwtToken = getAccessToken();
+
+  const [header, payload] = jwtToken.split(".");
+  const decodedPayload = JSON.parse(atob(payload));
+  const userId = decodedPayload.id;
   
-  if (!responseData || !Array.isArray(responseData) || responseData.length !== 2) {
+  if (!authorData || !Array.isArray(authorData) || authorData.length !== 2) {
     return <div>Loading...</div>; // Or handle loading state accordingly
   }
 
   const intId = parseInt(id);
+
+  console.log(intId);
   
-  const author = authorData?.find((obj) => obj.id === intId)?.email?.split("@")[0] || '';
+  
+  const author = authorData?.find((obj) => obj.id === userId)?.email?.split("@")[0] || '';
   const title = postData.find((obj) => obj.id === intId)['title'];
   const category = postData.find((obj) => obj.id === intId)['category'];
   const content = postData.find((obj) => obj.id === intId)['content'];
