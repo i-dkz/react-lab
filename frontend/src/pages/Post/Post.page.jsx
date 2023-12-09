1b7b7562429c72cb304fd4773f9293122cb6dc90
zach <102389340+zw0ng@users.noreply.github.com>
2023-12-08 23:20:32 -0800

    added loading skeleton with defer

--- frontend/src/pages/Post/Post.page.jsx
+++ frontend/src/pages/Post/Post.page.jsx
@@ -1,23 +1,63 @@
 import DOMAIN from "../../services/endpoint";
 import axios from "axios";
 import { ArticleCardImage } from "../../components/misc/ArticleCardImage";
-import { SimpleGrid, Container } from "@mantine/core";
-import { useLoaderData } from "react-router-dom";
+import { SimpleGrid, Container, Skeleton } from "@mantine/core";
+import { useLoaderData, Await, defer } from "react-router-dom";
+import React from "react";
+import classes from "./Post.module.css";
 
 export const PostPage = () => {
   const posts = useLoaderData();
+
   return (
-    <Container>
-      <SimpleGrid cols={3}>
-        {posts?.map((post) => (
-          <ArticleCardImage key={post.title} {...post} />
-        ))}
-      </SimpleGrid>
-    </Container>
+    <>
+      <Container>
+        <React.Suspense
+          fallback={
+            <Container className={classes.container}>
+              <Skeleton
+                color="blue"
+                height={15}
+                width={150}
+                radius={8}
+                mt={25}
+              />
+              <Skeleton
+                color="blue"
+                height={15}
+                width={150}
+                radius={8}
+                mt={20}
+              />
+              <Skeleton
+                color="blue"
+                height={30}
+                width={90}
+                radius={8}
+                mt={20}
+              />
+            </Container>
+          }
+        >
+          <Await
+            resolve={posts}
+            errorElement={<p>Error loading package location!</p>}
+          >
+            {(data) => (
+              <SimpleGrid cols={3}>
+                {data.posts?.map((post) => (
+                  <ArticleCardImage key={post.title} {...post} />
+                ))}
+              </SimpleGrid>
+            )}
+          </Await>
+        </React.Suspense>
+      </Container>
+    </>
   );
 };
 
 export const postsLoader = async () => {
   const res = await axios.get(`${DOMAIN}/api/posts`);
-  return res.data;
+  return defer({ posts: res.data });
 };
